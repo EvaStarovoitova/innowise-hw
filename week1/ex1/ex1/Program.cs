c2030a58fcf665e3b13b7cdc89794b4ebc66f4f2
Eva Starovoitova <ewa.starov@gmail.com>
2025-10-06 21:40:10 +0300

    commit

---
+++ week1/ex1/ex1/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+
+namespace ex1
+{
+    public interface ICalc
+    {
+        double Calc(double num1, double num2);
+    }
+
+    public class Sum : ICalc
+    {
+        public double Calc(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class Dev : ICalc
+    {
+        public double Calc(double num1, double num2)
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("Нельзя делить на ноль!");
+            }
+                      
+        }
+    }
+
+    public class Mul : ICalc
+    {
+        public double Calc(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+
+    public class Sub : ICalc
+    {
+        public double Calc(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+
+    class Calculator
+    {
+
+        public ICalc Calculate { get; set; }
+
+        public double Result(double num1, double num2)
+        {
+            return Calculate.Calc(num1, num2);
+        }
+    }
+
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator=new Calculator();
+            double result = 0;
+            double num1, num2;
+
+            while(true)
+            {
+                Console.WriteLine("Хотите ли вы использовать калькулятор (введите y или n)?");
+                string answer=Console.ReadLine();
+
+                if (answer.ToLower()=="y")
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Введите первое число:");
+
+                        if (double.TryParse(Console.ReadLine(), out num1))
+                        {
+                           
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введите число!");
+                        }
+                    }
+                   
+
+                    Console.WriteLine("Введите одну из операций  +, -, *, /:");
+                    string operation = Console.ReadLine();
+
+                    while (true)
+                    {
+                        Console.WriteLine("Введите второе число:");
+
+                        if (double.TryParse(Console.ReadLine(), out num2))
+                        {
+                           
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введите число!");
+                        }
+                    }
+                    
+
+                    try
+                    {
+                        switch (operation)
+                        {
+                            case "+":
+                                calculator.Calculate = new Sum();
+                                result = calculator.Result(num1, num2);
+                                Console.WriteLine($"Результат сложения {result}");
+                                break;
+                            case "-":
+                                calculator.Calculate = new Sub();
+                                result = calculator.Result(num1, num2);
+                                Console.WriteLine($"Результат вычитания {result}");
+                                break;
+                            case "*":
+                                calculator.Calculate = new Mul();
+                                result = calculator.Result(num1, num2);
+                                Console.WriteLine($"Результат умножения {result}");
+                                break;
+                            case "/":
+                                calculator.Calculate = new Dev();
+                                result = calculator.Result(num1, num2);
+                                Console.WriteLine($"Результат деления {result}");
+                                break;
+                            default:
+                                Console.WriteLine("Нет такой операции! Введите одну из предложенных операций!");
+                                break;
+                        }
+                    }
+                    catch(DivideByZeroException)
+                    {
+                        Console.WriteLine("Нельзя делить на ноль!");
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine($"Найдена ошибка:{ex.Message}");
+                    }
+                     
+                }
+                else if (answer.ToLower() == "n")
+                {
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Введите y или n");
+                }
+            } 
+        }
+    }
+}
---
+++ week1/ex2/ex2/ex2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        public string ProcessData(string dataName)
+        {
+            
+            Thread.Sleep(3000);
+            return $"Обработка {dataName} завершена за 3 секунды";
+        }
+
+        public async Task<string> ProcessDataAsync(string dataName)
+        {
+            await Task.Delay(3000);
+            return $"Обработка {dataName} завершена за 3 секунды";
+        }
+
+        static async Task Main(string[] args)
+        {
+            Program program= new Program();
+
+            Console.WriteLine("Синхронная обработка");
+
+            var start=DateTime.Now;
+
+            string res1 = program.ProcessData("Файл1");
+            Console.WriteLine(res1);
+
+            string res2 = program.ProcessData("Файл2");
+            Console.WriteLine(res2);
+
+            string res3 = program.ProcessData("Файл3");
+            Console.WriteLine(res3);
+
+            var resTime = DateTime.Now - start;
+            Console.WriteLine($"Синхронная обработка заняла {resTime}");
+
+            Console.WriteLine("Асинхронная обработка");
+
+            var startAsync = DateTime.Now;
+
+            Task<string> resAsync1 = program.ProcessDataAsync("Файл1");
+            Task<string> resAsync2 = program.ProcessDataAsync("Файл2");
+            Task<string> resAsync3 = program.ProcessDataAsync("Файл3");
+
+            await Task.WhenAll(resAsync1, resAsync2, resAsync3);
+
+            Console.WriteLine(resAsync1.Result);
+            Console.WriteLine(resAsync2.Result);
+            Console.WriteLine(resAsync3.Result);
+
+            var resTimeAsync = DateTime.Now - startAsync;
+            Console.WriteLine($"Асинхронная обработка заняла {resTimeAsync}");
+
+
+
+
+        }
+    }
+}
